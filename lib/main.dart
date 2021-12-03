c4417175c9e401274c20b884042cefb9783e5bab
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-12-03 18:01:16 +0100

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,17 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Image(
-          image: AssetImage('images/dice1.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset('images/dice1.png'),
+          ),
+          Expanded(
+            child: Image.asset('images/dice2.png'),
+          ),
+        ],
+      ),
     );
   }
 }
